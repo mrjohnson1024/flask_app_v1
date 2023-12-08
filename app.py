9f96f20c4b3a0ccc676a4c7a97f81700e7734ad6
mrjohnson1024 <23267553+mrjohnson1024@users.noreply.github.com>
2023-12-08 13:54:39 +0000

    added loop for items into html

--- app.py
+++ app.py
@@ -2,10 +2,32 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+LIST = [{
+    'id': 1,
+    'name': "First item",
+    'location': "Phoenix, AZ",
+    'qty': '47'
+}, {
+    'id': 2,
+    'name': "Second item",
+    'location': "Atlanta, GA",
+    'qty': '6874'
+}, {
+    'id': 3,
+    'name': "Third item",
+    'location': "Chicago, IL",
+    'qty': '11254'
+}]
+
 
 @app.route("/")
 def something():
-  return render_template('home.html')
+  return render_template('home.html', items=LIST)
+
+
+@app.route("/items")
+def items():
+  return "Here is a new route!"
 
 
 if __name__ == "__main__":
