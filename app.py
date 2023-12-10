c8f0652514e472d059822bd8462d4aa58b9f565a
mrjohnson1024 <23267553+mrjohnson1024@users.noreply.github.com>
2023-12-10 16:16:46 +0000

    catching up changes

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db, load_jobs_with_select
+from database import load_jobs_from_db, load_job_from_db, load_jobs_with_select
 
 app = Flask(__name__)
 
@@ -11,8 +11,22 @@ def something():
   return render_template('home.html', items=jobs)
 
 
+@app.route("/job/<id>")
+def show_job(id):
+  job = load_job_from_db(id)
+  if not job:
+    return "Not found", 404
+  return render_template('jobpage.html', job=job)
+
+
+@app.route("/api/job/<id>")
+def api_show_job(id):
+  job = load_job_from_db(id)
+  return jsonify(job)
+
+
 @app.route("/api/jobs")
-def list_LIST():
+def api_show_jobs():
   jobs = load_jobs_from_db()
   return jsonify(jobs)
 
